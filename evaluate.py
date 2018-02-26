b8273a08551b6d69e38c3bba7c2e5dc4123f2af3
Shariq Iqbal <shariqiqbal2810@users.noreply.github.com>
2018-02-25 16:24:51 -0800

    Added gif creation option to evaluation script

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,7 @@
 import argparse
 import torch
 import time
+import imageio
 import numpy as np
 from pathlib import Path
 from torch.autograd import Variable
@@ -17,6 +18,10 @@ def run(config):
     else:
         model_path = model_path / 'model.pt'
 
+    if config.save_gifs:
+        gif_path = model_path.parent / 'gifs'
+        gif_path.mkdir(exist_ok=True)
+
     maddpg = MADDPG.init_from_save(model_path)
     env = make_env(config.env_id, discrete_action=maddpg.discrete_action)
     maddpg.prep_rollouts(device='cpu')
@@ -25,6 +30,9 @@ def run(config):
     for ep_i in range(config.n_episodes):
         print("Episode %i of %i" % (ep_i + 1, config.n_episodes))
         obs = env.reset()
+        if config.save_gifs:
+            frames = []
+            frames.append(env.render('rgb_array')[0])
         env.render('human')
         for t_i in range(config.episode_length):
             calc_start = time.time()
@@ -37,11 +45,19 @@ def run(config):
             # convert actions to numpy arrays
             actions = [ac.data.numpy().flatten() for ac in torch_actions]
             obs, rewards, dones, infos = env.step(actions)
+            if config.save_gifs:
+                frames.append(env.render('rgb_array')[0])
             calc_end = time.time()
             elapsed = calc_end - calc_start
             if elapsed < ifi:
                 time.sleep(ifi - elapsed)
             env.render('human')
+        if config.save_gifs:
+            gif_num = 0
+            while (gif_path / ('%i_%i.gif' % (gif_num, ep_i))).exists():
+                gif_num += 1
+            imageio.mimsave(str(gif_path / ('%i_%i.gif' % (gif_num, ep_i))),
+                            frames, duration=ifi)
 
     env.close()
 
@@ -50,14 +66,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("env_id", help="Name of environment")
     parser.add_argument("model_name",
-                        help="Name of directory to store " +
-                             "model/training contents")
+                        help="Name of model")
     parser.add_argument("run_num", default=1, type=int)
+    parser.add_argument("--save_gifs", action="store_true",
+                        help="Saves gif of each episode into model directory")
     parser.add_argument("--incremental", default=None, type=int,
                         help="Load incremental policy from given episode " +
                              "rather than final policy")
     parser.add_argument("--n_episodes", default=10, type=int)
-    parser.add_argument("--episode_length", default=100, type=int)
+    parser.add_argument("--episode_length", default=25, type=int)
     parser.add_argument("--fps", default=30, type=int)
 
     config = parser.parse_args()
